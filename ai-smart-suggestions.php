ba25db8d5caba25c77bc2d35cc70cd5f112e1971
Hasmukh K Mistry <hasmukhmistry137@gmail.com>
2024-05-30 16:32:22 +0530

    Adds initial file for plugin

---
+++ ai-smart-suggestions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: AI Smart Suggestions
+ * Plugin URI: https://github.com/hasukmistry/ai-smart-suggestions
+ * Description: A plugin to add AI suggestions to your WordPress site content.
+ * Version: 1.0
+ * Requires at least: 6.2
+ * Requires PHP: 8.0
+ * Author: Hasmukh K Mistry
+ * Author URI: https://hasmukhmistry137.medium.com/
+ * License: GPL-2.0+
+ * License URI:
+ * Update URI:
+ * Text Domain: ai-smart-suggestions
+ * Domain Path:
+ */
+
+declare(strict_types=1);
